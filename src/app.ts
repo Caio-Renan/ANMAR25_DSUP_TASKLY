672a23d3b1c236bf4bcb2e78e8f6fc6fa604ae30
Caio Renan <caiorenan016@gmail.com>
2025-04-18 07:09:56 -0300

    feat(app): add noteRoutes and taskRoutes to main app entry

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 
+import taskRoutes from './routes/taskRoutes.js';
+import noteRoutes from './routes/noteRoutes.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/v1', taskRoutes);
+app.use('/api/v1', noteRoutes);
 
 export default app;
