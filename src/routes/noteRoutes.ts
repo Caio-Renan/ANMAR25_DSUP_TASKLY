c2a1e969a2f7ffea2e136a22338ad4f7ac6d5058
Caio Renan <caiorenan016@gmail.com>
2025-04-18 07:08:22 -0300

    feat(routes): add noteRoutes and taskRoutes to handle respective endpoints

--- src/routes/noteRoutes.ts
+++ src/routes/noteRoutes.ts
@@ -2,13 +2,23 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/tasks/:taskId/notes');
-router.post('/tasks/:taskId/notes');
+router.get('/tasks/:taskId/notes', (req, res) => {
+    res.send('');
+});
+router.post('/tasks/:taskId/notes', (req, res) => {
+    res.send('');
+});
 
-router.get('/notes/:id');
+router.get('/notes/:id', (req, res) => {
+    res.send('');
+});
 
-router.put('/notes/:id')
+router.put('/notes/:id', (req, res) => {
+    res.send('');
+});
 
-router.delete('/notes/:id');
+router.delete('/notes/:id', (req, res) => {
+    res.send('');
+});
 
 export default router;
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -2,15 +2,27 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/tasks');
-router.post('/tasks');
+router.get('/tasks', (req, res) => {
+    res.send('');
+});
+router.post('/tasks', (req, res) => {
+    res.send('');
+});
 
-router.get('/tasks/status/:status');
+router.get('/tasks/status/:status', (req, res) => {
+    res.send('');
+});
 
-router.get('/tasks/:id');
+router.get('/tasks/:id', (req, res) => {
+    res.send('');
+});
 
-router.put('/tasks/:id');
+router.put('/tasks/:id', (req, res) => {
+    res.send('');
+});
 
-router.delete('/tasks/:id');
+router.delete('/tasks/:id', (req, res) => {
+    res.send('');
+});
 
 export default router;
