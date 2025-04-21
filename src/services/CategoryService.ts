cd4575f5906ea0d7b508a763e1b00ae7a4748a31
Caio Renan <caiorenan016@gmail.com>
2025-04-21 16:05:23 -0300

    feat(services): implement initial services and serviceFactory to centralize service creation

---
+++ src/services/CategoryService.ts
@@ -0,0 +1,26 @@
+import { Category } from '@prisma/client';
+import { RepositoryFactory } from '@repositories/repositoryFactory';
+
+export class CategoryService {
+  private categoryRepository = RepositoryFactory.categoryRepository;
+
+  async create(data: Omit<Category, 'id' | 'createdAt' | 'updatedAt'>): Promise<Category> {
+    return this.categoryRepository.create(data);
+  }
+
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.findAll();
+  }
+
+  async findById(id: number): Promise<Category | null> {
+    return this.categoryRepository.findById(id);
+  }
+
+  async update(id: number, data: Partial<Category>): Promise<Category> {
+    return this.categoryRepository.update(id, data);
+  }
+
+  async delete(id: number): Promise<Category> {
+    return this.categoryRepository.delete(id);
+  }
+}
---
+++ src/services/NoteService.ts
@@ -0,0 +1,26 @@
+import { Note } from '@prisma/client';
+import { RepositoryFactory } from '@repositories/repositoryFactory';
+
+export class NoteService {
+  private noteRepository = RepositoryFactory.noteRepository;
+
+  async create(taskId: number, data: Omit<Note, 'id' | 'createdAt' | 'updatedAt'>): Promise<Note> {
+    return this.noteRepository.create({ ...data, taskId });
+  }
+
+  async findAllByTaskId(taskId: number): Promise<Note[]> {
+    return this.noteRepository.findAll({ taskId });
+  }
+
+  async findById(id: number): Promise<Note | null> {
+    return this.noteRepository.findById(id);
+  }
+
+  async update(id: number, data: Partial<Note>): Promise<Note> {
+    return this.noteRepository.update(id, data);
+  }
+
+  async delete(id: number): Promise<Note> {
+    return this.noteRepository.delete(id);
+  }
+}
